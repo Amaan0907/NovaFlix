85f90369e0897aaa829a677a58d2394de65fc7ef
Amaan0907 <mohdamaan00907@gmail.com>
2026-08-18 17:11:12 +0530

    update go.mod and go.sum to include new dependencies; add movie model with Genre, Ranking, and Movie structs

--- server/NovaFlixServer/main.go
+++ server/NovaFlixServer/main.go
@@ -1,11 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	
-	
+	router:=gin.Default()
 
+	router.GET("/hello",func(ctx *gin.Context) {
+		ctx.String(http.StatusOK,"Hello, NovaFlix")
+	})
+
+	if err:=router.Run(":3000");err!=nil{
+		fmt.Println("Failed to start server",err)
+		panic(err)
+	}
 
 
 }
