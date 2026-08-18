b8409dc61e82a99e0f2a64517c3e5cc30ad98f49
Amaan0907 <mohdamaan00907@gmail.com>
2026-08-18 23:39:43 +0530

    implement movie fetching functionality; add database connection with dotenv support; update go.mod and .gitignore

--- server/NovaFlixServer/controllers/movieController.go
+++ server/NovaFlixServer/controllers/movieController.go
@@ -1,10 +1,37 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"net/http"
+	"time"
 
+	"github.com/Amaan0907/NovaFlix/server/NovaFlixServer/database"
+	"github.com/Amaan0907/NovaFlix/server/NovaFlixServer/models"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+var movieCollection *mongo.Collection=database.OpenConnection("movies")
 func GetMovies() gin.HandlerFunc{
 	return func (c *gin.Context){
-		c.JSON(200,gin.H{"message":"List of Movies"})
+		ctx,cancel:=context.WithTimeout(context.Background(),100*time.Second)
+
+		defer cancel()
+		var movies []models.Movie
+		cursor,err:= movieCollection.Find(ctx, bson.M{})
+
+		if err!=nil{
+			c.JSON(http.StatusInternalServerError,gin.H{"Error":"Failed to fetch movies"})
+
+		}
+		defer cursor.Close(ctx)
+
+		if err=cursor.All(ctx,&movies);err!=nil{
+			c.JSON(http.StatusInternalServerError,gin.H{"Error":"Failed to decode movies"})
+
+		}
+		c.JSON(http.StatusOK,movies)
 	}
 }
 
---
+++ server/NovaFlixServer/database/databaseConnection.go
@@ -0,0 +1,53 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+func DbInstance() *mongo.Client{
+	err:=godotenv.Load(".env")
+
+	if err!=nil{
+		log.Println("Warning: unable to fund .env file")
+
+
+	}
+	MongoDB:=os.Getenv("MONGODB_URI")
+	if MongoDB==""{
+		panic("MONGODB_URI not set")
+	}
+	fmt.Println("MongoDB URI: ",MongoDB)
+
+	clientOptions:=options.Client().ApplyURI(MongoDB)
+
+	client,err:=mongo.Connect(clientOptions)
+	if err!=nil{
+		panic(err)
+	}
+
+	return client
+}
+
+var Client *mongo.Client=DbInstance()
+func OpenConnection(collectionName string) *mongo.Collection{
+	err:=godotenv.Load(".env")
+	if err!=nil{
+		log.Println("Warning: env do not exist")
+	}
+	databaseName:=os.Getenv("DATABASE_NAME")
+
+	fmt.Println("Database Name: ",databaseName)
+
+	collection:=Client.Database(databaseName).Collection(collectionName)
+
+	if collection==nil{
+		return nil
+	}
+	return collection
+}
