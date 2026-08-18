24f670e2f8bf81565f05fca3dd5eb60c915879f5
Amaan0907 <mohdamaan00907@gmail.com>
2026-08-18 22:31:27 +0530

    add movie controller with GetMovies function; update package name in movie model

--- server/NovaFlixServer/main.go
+++ server/NovaFlixServer/main.go
@@ -5,16 +5,18 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	controller "github.com/Amaan0907/NovaFlix/server/NovaFlixServer/controllers"
 )
 
 func main() {
 	
 	router:=gin.Default()
 
-	router.GET("/hello",func(ctx *gin.Context) {
-		ctx.String(http.StatusOK,"Hello, NovaFlix")
+	router.GET("/hello",func(c *gin.Context) {
+		c.String(http.StatusOK,"Hello, NovaFlix")
 	})
-
+	router.GET("/movies",controller.GetMovies())
+	
 	if err:=router.Run(":3000");err!=nil{
 		fmt.Println("Failed to start server",err)
 		panic(err)
